c82a95918a75b628e0391f6e6ef28114256ea4e1
aBleuCat <158527216+aBleuCat@users.noreply.github.com>
2026-02-10 20:49:25 -0500

    Update addrule.js

--- commands/addrule.js
+++ commands/addrule.js
@@ -1,43 +1,31 @@
 const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
-const fs = require('fs');
+const mongoose = require('mongoose');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('autorolechange')
-        .setDescription('Create a trigger to change roles temporarily')
-        .addUserOption(o => o.setName('messager').setDescription('User who sends message').setRequired(true))
-        .addUserOption(o => o.setName('target_user').setDescription('User mentioned').setRequired(true))
-        .addChannelOption(o => o.setName('channel').setDescription('Channel to monitor').setRequired(true))
-        .addRoleOption(o => o.setName('add_role').setDescription('Role to give temporarily').setRequired(true))
-        .addRoleOption(o => o.setName('restore_role').setDescription('Role to restore').setRequired(true))
-        .addIntegerOption(o => o.setName('duration').setDescription('Hours before revert').setRequired(true))
+        .setDescription('Add a role trigger rule')
+        .addUserOption(o => o.setName('messager').setDescription('Trigger user').setRequired(true))
+        .addUserOption(o => o.setName('target').setDescription('User to swap').setRequired(true))
+        .addChannelOption(o => o.setName('channel').setDescription('Channel').setRequired(true))
+        .addRoleOption(o => o.setName('add').setDescription('Temporary role').setRequired(true))
+        .addRoleOption(o => o.setName('restore').setDescription('Role to return').setRequired(true))
+        .addIntegerOption(o => o.setName('duration').setDescription('Hours').setRequired(true))
         .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
 
     async execute(interaction) {
-        const durationMs = interaction.options.getInteger('duration') * 60 * 60 * 1000;
-        
-        const rule = {
-            id: Date.now().toString().slice(-6), // Unique ID for removing later
+        const Rule = mongoose.model('Rule');
+        const newRule = new Rule({
+            ruleId: Date.now().toString().slice(-6),
             watchUser: interaction.options.getUser('messager').id,
-            targetUser: interaction.options.getUser('target_user').id,
+            targetUser: interaction.options.getUser('target').id,
             channel: interaction.options.getChannel('channel').id,
-            addRole: interaction.options.getRole('add_role').id,
-            restoreRole: interaction.options.getRole('restore_role').id,
-            durationMs: durationMs
-        };
+            addRole: interaction.options.getRole('add').id,
+            restoreRole: interaction.options.getRole('restore').id,
+            durationMs: interaction.options.getInteger('duration') * 60 * 60 * 1000
+        });
 
-        const path = './data/rules.json';
-        if (!fs.existsSync('./data')) fs.mkdirSync('./data');
-        
-        let rules = [];
-        if (fs.existsSync(path)) {
-            const data = fs.readFileSync(path, 'utf8');
-            rules = data ? JSON.parse(data) : [];
-        }
-
-        rules.push(rule);
-        fs.writeFileSync(path, JSON.stringify(rules, null, 2));
-
-        await interaction.reply({ content: `Rule saved! ID: **${rule.id}**`, ephemeral: true });
+        await newRule.save();
+        await interaction.reply({ content: `Rule saved to Cloud! ID: **${newRule.ruleId}**`, ephemeral: true });
     }
 };
