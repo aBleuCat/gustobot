b6f7249847236a1baa7b579dca2b11a1e595e23a
aBleuCat <158527216+aBleuCat@users.noreply.github.com>
2026-02-19 00:14:53 -0500

    Create dm.js

---
+++ commands/dm.js
@@ -0,0 +1,34 @@
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('dm')
+        .setDescription('Makes the bot DM a specific user')
+        .addUserOption(option => 
+            option.setName('user')
+                .setDescription('The user to message')
+                .setRequired(true))
+        .addStringOption(option => 
+            option.setName('message')
+                .setDescription('The message to send')
+                .setRequired(true))
+
+    async execute(interaction) {
+        const targetUser = interaction.options.getUser('user');
+        const messageText = interaction.options.getString('message');
+
+        try {
+            await targetUser.send(messageText);
+            await interaction.reply({ 
+                content: `Successfully sent message to **${targetUser.displayName}**.`, 
+                ephemeral: true 
+            });
+        } catch (error) {
+            console.error(error);
+            await interaction.reply({ 
+                content: `I couldn't DM **${targetUser.displayName}**. They might have their DMs closed.`, 
+                ephemeral: true 
+            });
+        }
+    },
+};
