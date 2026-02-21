2f0734169089e1a0d754ca572b685b84f666e1d6
aBleuCat <158527216+aBleuCat@users.noreply.github.com>
2026-02-21 01:29:24 -0500

    Update banadvice.js

--- commands/banadvice.js
+++ commands/banadvice.js
@@ -1,15 +1,40 @@
-const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+const { SlashCommandBuilder } = require('discord.js');
+const mongoose = require('mongoose');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('banadvice')
-        .setDescription('Bans or unbans a user from using the advicegive command.')
+        .setDescription('Bans or unbans a user from using the advicegive command (Owner Only)')
         .addUserOption(option => 
             option.setName('user')
                 .setDescription('The user to ban/unban')
-                .setRequired(true))
-        .setDefaultMemberPermissions(PermissionFlagsBits.Administrator), // Only admins can see/use this
+                .setRequired(true)),
+
     async execute(interaction) {
-        // Logic is handled in index.js
+        // Owner ID Check
+        if (interaction.user.id !== '934290747623096381') {
+            return interaction.reply({ 
+                content: 'You do not have permission to use this command.', 
+                ephemeral: true 
+            });
+        }
+
+        const AdviceBan = mongoose.model('AdviceBan');
+        const target = interaction.options.getUser('user');
+
+        try {
+            const exists = await AdviceBan.findOne({ userId: target.id });
+
+            if (exists) {
+                await AdviceBan.deleteOne({ userId: target.id });
+                return interaction.reply(`Unbanned **${target.username}** from giving advice.`);
+            } else {
+                await new AdviceBan({ userId: target.id }).save();
+                return interaction.reply(`Banned **${target.username}** from giving advice.`);
+            }
+        } catch (error) {
+            console.error(error);
+            return interaction.reply({ content: 'Error updating advice ban status.', ephemeral: true });
+        }
     },
 };
