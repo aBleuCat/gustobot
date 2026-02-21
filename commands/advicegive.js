fefadd64d825bcf1fadeeed2b070ee646b5474a3
aBleuCat <158527216+aBleuCat@users.noreply.github.com>
2026-02-20 23:52:48 -0500

    Update advicegive.js so that it can be used in user installs

--- commands/advicegive.js
+++ commands/advicegive.js
@@ -5,6 +5,8 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName('advicegive')
         .setDescription('Add a piece of advice to the goon circle of advice')
+        .setContexts([0, 1, 2]) 
+        .setIntegrationTypes([0, 1]) 
         .addStringOption(option => 
             option.setName('text')
                 .setDescription('The advice you want to give')
