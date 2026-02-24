a676ec4aaf365b6fa2d1bc1bbb1a7880d46c19f0
aBleuCat <158527216+aBleuCat@users.noreply.github.com>
2026-02-24 16:42:02 -0500

    Update abortbaby.js

--- guild_commands/abortbaby.js
+++ guild_commands/abortbaby.js
@@ -3,7 +3,14 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('abortbaby')
-        .setDescription('abortion is still legal here dw'),
+        .setDescription('Abortion is still legal here dw'),
     async execute(interaction) {
+        const roleId = '1473123914531213532';
+        try {
+            await interaction.member.roles.remove(roleId);
+            return interaction.reply({ content: 'Baby aborted' });
+        } catch (e) {
+            return interaction.reply({ content: 'I couldn\'t abort the baby for you', ephemeral: true });
+        }
     },
 };
