4aec7c681c2d7b735de14b707afce7483a6de81a
aBleuCat <158527216+aBleuCat@users.noreply.github.com>
2026-02-25 00:49:27 -0500

    Create mutelolall.js

---
+++ commands/mutelolall.js
@@ -0,0 +1,39 @@
+const { SlashCommandBuilder, PermissionFlagsBits, ChannelType } = require('discord.js');
+const mongoose = require('mongoose');
+
+// get existing local mute model
+const MutedChannel = mongoose.models.MutedChannel || mongoose.model('MutedChannel', new mongoose.Schema({ channelId: String }));
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('mutelolall')
+        .setDescription('mutes triggers in all channels')
+        .addBooleanOption(o => o.setName('status').setDescription('true to mute, false to unmute').setRequired(true))
+        .addChannelOption(o => o.setName('exception').setDescription('channel to ignore')),
+    async execute(interaction) {
+        // check perms
+        if (!interaction.member.permissions.has(PermissionFlagsBits.ManageChannels)) {
+            return interaction.reply({ content: 'you have no permission to do this', flags: [1 << 6] });
+        }
+
+        const status = interaction.options.getBoolean('status');
+        const exception = interaction.options.getChannel('exception');
+        const channels = interaction.guild.channels.cache.filter(c => c.type === ChannelType.GuildText);
+
+        if (status) {
+            // mute all
+            for (const [id, channel] of channels) {
+                if (exception && id === exception.id) continue;
+                await MutedChannel.findOneAndUpdate({ channelId: id }, { channelId: id }, { upsert: true });
+            }
+            return interaction.reply(`muted all channels ${exception ? `except ${exception.name}` : ''}`);
+        } else {
+            // unmute all
+            for (const [id, channel] of channels) {
+                if (exception && id === exception.id) continue;
+                await MutedChannel.deleteOne({ channelId: id });
+            }
+            return interaction.reply('unmuted all channels');
+        }
+    },
+};
