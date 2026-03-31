743ac6b9282e2ab4102a3ff4c777564152a81247
webcubed <77589042+webcubed@users.noreply.github.com>
2026-03-30 21:43:35 -0400

    replace diff w/ patch

--- commands/horsegamble.js
+++ commands/horsegamble.js
@@ -136,7 +136,7 @@ function formatCycleLog(cycleNum, horseLabel, result, bankedThisCycle, coinsAfte
 
     return (
         `**Cycle #${cycleNum}**\n` +
-        '```diff\n' +
+        '```patch\n' +
         `- Gambled: ${totalGambled} ${horseLabel}\n` +
         `+ Wins:    ${wins}\n` +
         `- Losses:  ${losses}\n` +
@@ -299,7 +299,7 @@ module.exports = {
                 return interaction.reply({
                     content:
                         `**Horse Coin Gamble**\n\n` +
-                        '```diff\n' +
+                        '```patch\n' +
                         `- 2 🪙 → +${winAmount} 🪙${testTag}\n` +
                         '```',
                 });
@@ -319,7 +319,7 @@ module.exports = {
             return interaction.reply({
                 content:
                     `**Horse Coin Gamble**\n\n` +
-                    '```diff\n' +
+                    '```patch\n' +
                     `- Gambled: ${gamblesCount} 🪙\n` +
                     `+ Wins:    ${wins}\n` +
                     `- Losses:  ${losses}\n` +
@@ -795,7 +795,7 @@ module.exports = {
 
         const summary = [
             `**Horse Gamble Results**`,
-            '```diff',
+            '```patch',
             `- Gambled: ${totalGambled} ${horseLabel}`,
             `+ Wins:    ${totalWins}`,
             `- Losses:  ${totalLosses}`,
