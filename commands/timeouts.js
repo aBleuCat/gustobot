c8725578914ddefc4fde6e9a3e1cca51737388e2
aBleuCat <choudhurytahera06@gmail.com>
2026-05-05 17:34:55 -0400

    add horse of testicular cancer, unicorn

--- commands/timeouts.js
+++ commands/timeouts.js
@@ -1,30 +1,30 @@
-const { SlashCommandBuilder, EmbedBuilder, PermissionFlagsBits, MessageFlags } = require('discord.js');
+const {SlashCommandBuilder, EmbedBuilder, PermissionFlagsBits, MessageFlags} = require('discord.js');
 const mongoose = require('mongoose');
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('timeouts')
-        .setDescription('View all active role-swaps')
-        .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages),
+	data: new SlashCommandBuilder()
+		.setName('timeouts')
+		.setDescription('View all active role-swaps')
+		.setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages),
 
-    async execute(interaction) {
-        const Timeout = mongoose.model('Timeout');
-        const activeTimeouts = await Timeout.find({}).lean();
+	async execute(interaction) {
+		const Timeout = mongoose.model('Timeout');
+		const activeTimeouts = await Timeout.find({}).lean();
 
-        if (activeTimeouts.length === 0) {
-            return interaction.reply({ content: "There are no active role-swap timeouts.", flags: [MessageFlags.Ephemeral] });
-        }
+		if (activeTimeouts.length === 0) {
+			return interaction.reply({content: 'There are no active role-swap timeouts.', flags: [MessageFlags.Ephemeral]});
+		}
 
-        const embed = new EmbedBuilder()
-            .setTitle('⏳ Active Role Timeouts')
-            .setColor('#6463FA');
+		const embed = new EmbedBuilder()
+			.setTitle('⏳ Active Role Timeouts')
+			.setColor('#6463FA');
 
-        const list = activeTimeouts.map(t => {
-            const unixSeconds = Math.floor(t.revertAt / 1000);
-            return `<@${t.targetUser}>: Has <@&${t.addRole}>, restores to <@&${t.restoreRole}> <t:${unixSeconds}:R>`;
-        }).join('\n');
+		const list = activeTimeouts.map(t => {
+			const unixSeconds = Math.floor(t.revertAt / 1000);
+			return `<@${t.targetUser}>: Has <@&${t.addRole}>, restores to <@&${t.restoreRole}> <t:${unixSeconds}:R>`;
+		}).join('\n');
 
-        embed.setDescription(list);
-        await interaction.reply({ embeds: [embed] });
-    },
+		embed.setDescription(list);
+		await interaction.reply({embeds: [embed], flags: [MessageFlags.Ephemeral]});
+	},
 };
